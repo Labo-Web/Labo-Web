f48b73169c95852e8c4d3c00da3e2bd15d560a73
aubindesign <aubin.lorieux@supinfo.com>
2012-03-29 22:53:39 +0100

    Ré-écriture et explication du code pour la lisibilité.

--- IA Project/Game/Client/js/main.js
+++ IA Project/Game/Client/js/main.js
@@ -1,64 +1,104 @@
+//Déclaration l'objet Engine
 var Engine = {};
+
+//Déclaration d'un objet Map
 Engine.Map = {};
-Engine.Param =  {};
-Engine.Camera ={};
-Engine.FpsT=0;
-Engine.Fps=0;
+
+//Déclaration d'un objet Param
+Engine.Param = {};
+
+//Déclaration d'un objet Camera
+Engine.Camera = {};
+
+//Déclaration d'un tableau pour les images
 Engine.Tiles = new Array();
-Engine.ChangeCase= function(elm)
-{
-var clickX = window.event.x-elm.offsetLeft;
-var clickY = window.event.y-elm.offsetTop;
-var CaseX= Math.floor(clickX/Engine.Param.CaseSize);
-var CaseY= Math.floor(clickY/Engine.Param.CaseSize);
-Engine.Map.cases[CaseY][CaseX]++;
-var Case=Engine.Map.cases[CaseY][CaseX];
-var texture = Engine.Tiles[Case];
-if(texture==undefined)
-	Engine.Map.cases[CaseY][CaseX]=0;
-//alert(clickX+"/"+ clickY);
-Engine.DrawMap();
-};
+
+//Function lancée quand la page est chargée (onload dans HTML)
 function init(){
+	//Déclaration de la variable séléctionnant l'élément canvas
     var canvas = document.getElementById('gameframe');
+
+	//Permet de tester si navigateur capable de faire tourner du canvas
     if (canvas.getContext){
-        //parameters
+        
+		//Parameters
+		
+		//Stock le context du canvas dans un objet
         Engine.ctx = canvas.getContext('2d'); 
+		
+		//Définie la largeur et hauteur du canvas
         Engine.Param.CanvasW = parseInt(canvas.getAttribute("width")) ;
         Engine.Param.CanvasH = parseInt(canvas.getAttribute("height"));
-
+		
+		//Définie la taille d'une case
         Engine.Param.CaseSize=10
         
+		//Définie les différentes images pour les textures
         Engine.Tiles[0] = new Image();
-        Engine.Tiles[0].src = './img/sand.jpg';
+        Engine.Tiles[0].src = './img/grass.jpg';
         Engine.Tiles[1] = new Image();
         Engine.Tiles[1].src = './img/road.jpg';      
 		Engine.Tiles[2] = new Image();
-        Engine.Tiles[2].src = './img/grass.jpg';
+        Engine.Tiles[2].src = './img/sand.jpg';
 		Engine.Tiles[3] = new Image();
         Engine.Tiles[3].src = './img/tire.jpg';
 		
+		//On lance la méthode DrawMap
 		Engine.DrawMap();
 		}
 }
-Engine.DrawMap= function ()
-{
 
-for (y = 0; y <29 ; y++) {
-        for (x = 0; x < 20; x++) {
-		var texture;
-		var Case= 0; 
-		try{
-						Case=Engine.Map.cases[y][x];
-                        texture = Engine.Tiles[Case];
-                        img=true;
-                    }
-					  catch(e){texture = Engine.Tiles[0];};
-			Engine.ctx.drawImage(texture,x*Engine.Param.CaseSize,y*Engine.Param.CaseSize)
-		}
-		}
+//Methode de modification de la case au clic(appeller dans le html)
+Engine.ChangeCase = function(elm) {
+	
+	//Déclare une variable x, y définissant la quand on clique la valeur x dans la fenêtre
+	var clickX = window.event.x-elm.offsetLeft;
+	var clickY = window.event.y-elm.offsetTop;
+	
+	//Retourne l'entier inférieur(floor) x, y de la case
+	var CaseX = Math.floor(clickX/Engine.Param.CaseSize);
+	var CaseY = Math.floor(clickY/Engine.Param.CaseSize);
+	
+	//Incrément dans l'objet Engine.Map par coordonnée
+	Engine.Map.cases[CaseY][CaseX]++;
+	
+	//Déclaration de la variable case comprennant l'int du type de case
+	var Case = Engine.Map.cases[CaseY][CaseX];
+	
+	//Déclaration de la variable récupérant la texture
+	var texture = Engine.Tiles[Case];
+	
+	//Test permettant de déterminer si pas de texture définir la texture par défaut : 0
+	if(texture == undefined)
+		Engine.Map.cases[CaseY][CaseX]=0;
+	Engine.DrawMap();
 };
-Engine.DumpMap = function ()
-{
+
+//Méthode permettant de contruire la map dans le canvas au chargement de la page
+Engine.DrawMap= function () {
+	//Boucle permettant de parcourir la hauteur du canvas
+	for (y = 0; y < 29 ; y++) {
+		//Boucle permettant de parcourir la hauteur du canvas
+        for (x = 0; x < 20; x++) {
+			//Déclaration de la texture
+			var texture;
+			//Déclaration de la case 
+			var Case = 0; 
+			
+			//Try catch : 
+			// - Récupére la valeur du json y,x
+			// - récupère la texture en fonction de la valeur du json
+			try{
+				Case = Engine.Map.cases[y][x];
+                texture = Engine.Tiles[Case];
+             }
+			catch(e){texture = Engine.Tiles[0];};
+				Engine.ctx.drawImage(texture,x*Engine.Param.CaseSize,y*Engine.Param.CaseSize)
+		}
+	}
+};
+
+//Function permettant de générer un json en fonction des textures de la map
+Engine.DumpMap = function () {
 	document.getElementById('gameframe').innerHTML=JSON.stringify(Engine.Map);
 }
