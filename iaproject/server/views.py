a98eaa5251c83868a16e82551d47eb8d3b2eff84
Damien PUIG <puigfb@gmail.com>
2012-05-31 23:41:44 +0100

    mise en place des prises en comtpe GET POST pour ia

--- iaproject/server/views.py
+++ iaproject/server/views.py
@@ -1,9 +1,12 @@
 #coding=utf-8
 from iaproject.server.gamesocket import GameNamespace
+from pyramid.response import Response
+from pyramid.view import view_config
 from socketio import socketio_manage
 
 def index(request):
     """ Base view to load our template """
+    
     return {}
 
 def socketio_service(request):
@@ -15,3 +18,12 @@ def socketio_service(request):
     )
 
     return retval
+
+@view_config(route_name='ia',request_method='GET')
+def ia(request):
+    return Response('damien')
+
+@view_config(route_name='ia',request_method='POST')
+def test_ia(request):
+    print request.POST['id']
+    return Response(str(request.GET['id']))
